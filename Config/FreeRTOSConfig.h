07c4375025604b5cbdc8faa24e85d6aa9c89e3c3
Kevin Gerber <git@gaerber.ch>
2014-06-02 11:43:34 +0200

    Added memory pool service for freeRTOS by BFH.

--- Config/FreeRTOSConfig.h
+++ Config/FreeRTOSConfig.h
@@ -84,14 +84,13 @@
 extern uint32_t SystemCoreClock;
 
 /* Demo only. Must be removed! */
-#define configENABLE_BACKWARD_COMPATIBILITY		0
+#define configENABLE_BACKWARD_COMPATIBILITY		1	/* Need for the memory pool service */
 
 #define configUSE_PREEMPTION			1
 #define configUSE_IDLE_HOOK				0
 #define configUSE_TICK_HOOK				0
 #define configCPU_CLOCK_HZ				( SystemCoreClock )
 #define configTICK_RATE_HZ				( ( TickType_t ) 1000 )
-#define portTICK_RATE_MS				1
 #define configMAX_PRIORITIES			( 5 )
 #define configMINIMAL_STACK_SIZE		( ( unsigned short ) 130 )
 #define configTOTAL_HEAP_SIZE			( ( size_t ) ( 75 * 1024 ) )
