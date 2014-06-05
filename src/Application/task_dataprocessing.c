50bc48e7d27d9a7954af5e15218fbdd7304ca77c
Kevin Gerber <git@gaerber.ch>
2014-06-05 15:24:36 +0200

    Base64 encoder.

--- src/Application/task_dataprocessing.c
+++ src/Application/task_dataprocessing.c
@@ -120,7 +120,7 @@ void taskDataProcessing(void* pvParameters) {
  */
 inline void dataEncode(int16_t azimuth, int16_t distance, char *base64) {
 	/* Look up table due to performance */
-	static const look_up_table[] = {
+	static const char look_up_table[] = {
 			'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
 			'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
 			'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
@@ -130,11 +130,11 @@ inline void dataEncode(int16_t azimuth, int16_t distance, char *base64) {
 
 	/* Convert azimuth */
 	base64[0] = look_up_table[(azimuth >> 6) & 0x3F];
-	base64[0] = look_up_table[(azimuth) & 0x3F];
+	base64[1] = look_up_table[(azimuth) & 0x3F];
 
 	/* Convert distance */
-	base64[0] = look_up_table[(distance >> 6) & 0x3F];
-	base64[0] = look_up_table[(distance) & 0x3F];
+	base64[2] = look_up_table[(distance >> 6) & 0x3F];
+	base64[3] = look_up_table[(distance) & 0x3F];
 }
 
 
