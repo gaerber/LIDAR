fed40e1d832ef8fe4079c03cb225fb0aed3b32a1
Kevin Gerber <git@gaerber.ch>
2014-06-11 13:50:53 +0200

    TDC interrupt source configured.

--- Libraries/BSP/inc/bsp_gp22.h
+++ Libraries/BSP/inc/bsp_gp22.h
@@ -40,7 +40,7 @@ typedef void (*bsp_gp22callback_t)(void);
  */
 #define BSP_GP22_REG0		0x00042055		/*!< TDC-GP22 register 0 configuration. */
 #define BSP_GP22_REG1		0x194900AA		/*!< TDC-GP22 register 1 configuration. */
-#define BSP_GP22_REG2		0xC0000000		/*!< TDC-GP22 register 2 configuration. */
+#define BSP_GP22_REG2		0xA0000000		/*!< TDC-GP22 register 2 configuration. */
 #define BSP_GP22_REG3		0x200000FF		/*!< TDC-GP22 register 3 configuration. */ /* EN_ERR_VAL FUNKTIONIERT NICHT WIE ERWARTET */
 #define BSP_GP22_REG4		0x20000000		/*!< TDC-GP22 register 4 configuration. */
 #define BSP_GP22_REG5		0x10000000		/*!< TDC-GP22 register 5 configuration. */
